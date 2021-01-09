47c46e4125e8e314259e198623f01f7fafa007fa
Siam <59419979@qq.com>
2021-01-09 20:25:56 +0800

    fixes:部分逻辑

--- esPlugsConfig.php
+++ esPlugsConfig.php
@@ -1,12 +1,12 @@
 <?php
 return [
     'name'=>'server服务管理',
-    'des'=>'server主服务的管理模块',
+    'des'=>'server主服务到管理模块',
     'namespace'=>'Siam\\ServerManagePlugs\\',
-    'version'=>'1.0',
+    'version'=>'1.0',//当前插件版本号,
     'logo'=>'https://s3.ax1x.com/2020/12/08/DzOz8S.jpg',
     'home' => [
         'type' => 0,
-        'url' => '/Siam/ServerManagePlugs/index'
+        'url' => '/siam/server-manage-plugs/index'
     ]
 ];
